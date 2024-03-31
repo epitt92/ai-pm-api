e467ac79987be53f099089ca3e288ca90d52ab24
epitt92 <info.epitt92@gmail.com>
2024-03-31 11:57:37 -0700

    update

--- aisite/aisite/wsgi.py
+++ aisite/aisite/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
+import os
+import sys
+
+# Add the parent directory of the current file (wsgi.py) to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aisite.settings')
 
 application = get_wsgi_application()
+
